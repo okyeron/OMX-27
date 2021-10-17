35799d5624d2dad5aed0cad78fd7829ce8e3fa8a
Chris Atkins <catkins@zendesk.com>
2021-10-17 21:46:36 +1100

    formatting of trig conditions

--- OMX-27-firmware/sequencer.h
+++ OMX-27-firmware/sequencer.h
@@ -38,7 +38,6 @@ class SequencerState {
 public:
   // TODO: is this needed by other modes?
   int midiChannel;                // the MIDI channel number to send messages
-
   int ticks;                      // A tick of the clock
   bool clockSource;               // Internal clock (0), external clock (1)
   bool playing;                   // Are we playing?
@@ -196,9 +195,16 @@ int loopCount[NUM_PATTERNS][NUM_STEPS] = {
 	{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 };
 
-const char* trigConditions[36] = {"1:1","1:2","2:2","1:3","2:3","3:3","1:4","2:4","3:4","4:4","1:5","2:5","3:5","4:5","5:5","1:6","2:6","3:6","4:6","5:6","6:6","1:7","2:7","3:7","4:7","5:7","6:7","7:7","1:8","2:8","3:8","4:8","5:8","6:8","7:8","8:8"};
-int ABcondition = 0;
-int trigConditionsAB[36][2] ={
+const char* trigConditions[36] = {
+  "1:1",
+  "1:2","2:2",
+  "1:3","2:3","3:3",
+  "1:4","2:4","3:4","4:4",
+  "1:5","2:5","3:5","4:5","5:5",
+  "1:6","2:6","3:6","4:6","5:6","6:6",
+  "1:7","2:7","3:7","4:7","5:7","6:7","7:7",
+  "1:8","2:8","3:8","4:8","5:8","6:8","7:8","8:8"};
+int trigConditionsAB[36][2] = {
 	{1,1},
 	{1,2}, {2,2},
 	{1,3}, {2,3}, {3,3},
